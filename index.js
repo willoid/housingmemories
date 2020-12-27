56bf5be94a7d1edc539611fee3040518154a2628
WILLOID <socialguille@gmail.com>
2020-12-27 18:23:47 +0100

    first mobile layout

---
+++ index.js
@@ -0,0 +1,18 @@
+/*slideshow upon https://www.w3schools.com/howto/howto_js_slideshow.asp */
+
+var slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+    showSlides(slideIndex += n);
+}
+
+function showSlides(n) {
+    var i;
+    var slides = document.getElementsByClassName("slides");
+    if (n > slides.length) {slideIndex = 1}
+    if (n < 1) {slideIndex = slides.length}
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display= "none";}
+    slides[slideIndex-1].style.display = "block";
+}
